c8435f8af23ee1ba75a4549ba43f6290cf131dee
Felipe Cesar Soberanski <felepeipe@gmail.com>
2021-01-26 18:42:53 -0300

    Add files via upload

---
+++ Entities/Department.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Exericicio.Entities
+{
+    class Department
+    {
+        public string deptName { get; set; }
+        public Department(object deptName)
+        {
+            deptName = deptName;
+        }
+    }
+}
---
+++ Entities/enums/WorkerLevel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exericicio.Entities.enums
+{
+    enum WorkerLevel : int
+    {
+        JUNIOR = 0,
+        MID_LEVEL = 1,
+        SENIOR = 2
+    }
+}
